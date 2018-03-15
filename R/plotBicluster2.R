5b0c7691606cbbccfa4d812103909a8cfb0b2ae4
danvilar <danvjmusic@gmail.com>
2018-03-15 16:42:49 +0000

    plotBicluster2 corrections

--- R/plotBicluster2.R
+++ R/plotBicluster2.R
@@ -1,4 +1,4 @@
-plotBicluster2 <- function(x,dataset, label=1, col=gray(seq(from=1,to=0,length=100))){
+plotBicluster2 <- function(x,dataset, label=1, col=gray(seq(from=1,to=0,length=50))){
     library(gplots)
 
     rainbow12equal = c("#BF4D4D",
@@ -14,14 +14,18 @@ plotBicluster2 <- function(x,dataset, label=1, col=gray(seq(from=1,to=0,length=1
         "#BF4DBF", 
         "#BF4D86")
     
-    heatmap.2(dataset[x$features,c(x$samples,setdiff(1:ncol(dataset),x$samples))],
-        col=col,Colv=NA,
-        Rowv=NA,
-        ColSideColors=c(rep("black",length(x$samples)),rep("white",ncol(dataset)-length(x$samples))),
+    sampleOrder = c(x$samples,setdiff(1:ncol(dataset),x$samples))    
+
+    heatmap.2(dataset[x$features, sampleOrder],
+        col = col,
+        dendrogram = 'none',
+        Colv = NA,
+        #Rowv = NA,
+        ColSideColors = c(rep("black",length(x$samples)),rep("white",ncol(dataset)-length(x$samples))),
         trace = "none", 
-        colCol = rainbow12equal[label+1],
+        colCol = rainbow12equal[label[sampleOrder]+1],
         key = F,
-        lwid=c(0.1,4),
-        lhei=c(0.1,4)
+        lwid = c(0.1,4),
+        lhei = c(0.1,4)
     )
 }
